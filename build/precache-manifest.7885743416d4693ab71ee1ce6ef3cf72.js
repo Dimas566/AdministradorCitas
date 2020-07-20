c4494526fe8d8b85e784f662b9f78a15213416ed
Dimas Garcia <josdimasgarcia@yahoo.com>
2020-07-19 20:40:04 -0600

    Primer Commit

---
+++ build/precache-manifest.7885743416d4693ab71ee1ce6ef3cf72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2865c6bcef8811863166f708f27ed3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c951fa9f47aa14cd1814",
+    "url": "/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "0012f5a03a153668762a",
+    "url": "/static/js/2.2aca169f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2aca169f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c951fa9f47aa14cd1814",
+    "url": "/static/js/main.b523b313.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
